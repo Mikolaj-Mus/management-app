ed278ff8893bf0d2c2ae276e0b1a748c191cc8e7
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-12-02 14:50:29 +0100

    Reacreate sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import NoProjects from "./components/NoProjects";
-import YourProjects from "./components/YourProjects";
-import CreateProject from "./components/CreateProject";
-import ProjectView from "./components/ProjectView";
-import { useState } from "react";
+import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
-  const [site, setSite] = useState(
-    <NoProjects onChange={handleCreateProject} />
-  );
-
-  function handleCreateProject() {
-    setSite(<CreateProject onChange={handleProjectView} />);
-  }
-
-  function handleProjectView() {
-    setSite(<ProjectView />);
-  }
-
   return (
-    <main className="w-screen h-screen flex">
-      <YourProjects />
-      {site}
+    <main className="h-screen my-8 ">
+      <ProjectsSidebar />
     </main>
   );
 }
