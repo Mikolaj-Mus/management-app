66ab26a5bca637bf629378639f776af2b525c628
Mikolaj-Mus <mus_mikolaj@wp.pl>
2023-12-02 15:41:55 +0100

    Component modification

---
+++ src/components/NoProjectSelected.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/no-projects.png";
+import Button from "./Button";
+
+export default function NoProjectSelected({ onStartAddProject }) {
+  return (
+    <div className="mt-24 text-center w-2/3">
+      <img src={logo} alt="Book with a pen" className="h-16 w-16 mx-auto" />
+      <h2 className="text-stone-500 font-bold text-xl mb-5 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4">
+        Select a project or get started with a new one
+      </p>
+      <p className="mt-8">
+        <Button onClick={onStartAddProject}>Create new project</Button>
+      </p>
+    </div>
+  );
+}
